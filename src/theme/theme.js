d6920115ea64a1d850855815e7641f80b770ff91
Rolando Orellana <orellana.marketing@gmail.com>
2024-08-29 17:49:55 -0600

    Implement Material-UI in byteCode website

---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#F4F4F4",
+    },
+    text: {
+      primary: "#010101",
+    },
+    primary: {
+      main: "#DDF247",
+    },
+  },
+  typography: {
+    fontFamily: "Source Code Pro, monospace",
+    button: {
+      fontFamily: "Aeonik, sans-serif",
+    },
+  },
+});
+
+export default theme;
