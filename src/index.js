072deab286a444f598a5edc209ba50933356877f
Rolando Orellana <orellana.marketing@gmail.com>
2024-09-04 17:21:26 -0600

    Removed commented lines of code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importa desde 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/theme';
 import './config/i18n';
 
-// Obtén el contenedor 'root' del DOM
 const rootElement = document.getElementById('root');
 
-// Crea el root usando createRoot
 const root = ReactDOM.createRoot(rootElement);
 
-// Renderiza la app usando createRoot
 root.render(
   <ThemeProvider theme={theme}>
     <App />
