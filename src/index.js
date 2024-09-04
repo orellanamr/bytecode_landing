68db0233c193dcccc544d904a3471b65f0d0743d
Rolando Orellana <orellana.marketing@gmail.com>
2024-09-04 16:55:36 -0600

    Updated index.js to use React 18 createRoot API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Importa desde 'react-dom/client'
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/theme';
 import './config/i18n';
 
-ReactDOM.render(
+// Obtén el contenedor 'root' del DOM
+const rootElement = document.getElementById('root');
+
+// Crea el root usando createRoot
+const root = ReactDOM.createRoot(rootElement);
+
+// Renderiza la app usando createRoot
+root.render(
   <ThemeProvider theme={theme}>
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
