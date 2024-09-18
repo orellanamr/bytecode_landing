dc61adb39d774b6c9e9791b9501585942151b9a0
Rolando Orellana <orellana.marketing@gmail.com>
2024-09-18 15:53:56 -0600

    Add FeatureSection component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './components/organisms/HeroSection/HeroSection';
 import TechCarousel from './components/organisms/TechCarousel/TechCarousel';
+import FeaturesSection from './components/organisms/FeaturesSection/FeaturesSection';
 
 function App() {
   return (
     <div>
       <HeroSection />
       <TechCarousel />
+      <FeaturesSection />
     </div>
   );
 }
